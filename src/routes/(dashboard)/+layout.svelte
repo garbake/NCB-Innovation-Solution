08940700fd19e5cc59163afb29b5944566b68b5f
ReinerYaeger <chevking101@gmail.com>
2023-08-02 11:15:27 -0500

    Added new routes

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+    import logo from '$lib/images/Ncb_Logo_Final.png';
+
     let sidebarOpen = false;
 
     function toggleSidebar() {
@@ -11,54 +13,16 @@
     <aside
         class="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-white"
     >
-        <div class="sidebar-header flex items-center justify-center py-4">
+        <div class="sidebar-header  bold flex items-center justify-center py-4">
             <div class="inline-flex">
-                <a href="#" class="inline-flex flex-row items-center">
-                    <svg
-                        class="w-10 h-10 text-red-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M11.757 2.034a1 1 0 01.638.519c.483.967.844 1.554 1.207 2.03.368.482.756.876 1.348 1.467A6.985 6.985 0 0117 11a7.002 7.002 0 01-14 0c0-1.79.684-3.583 2.05-4.95a1 1 0 011.707.707c0 1.12.07 1.973.398 2.654.18.374.461.74.945 1.067.116-1.061.328-2.354.614-3.58.225-.966.505-1.93.839-2.734.167-.403.356-.785.57-1.116.208-.322.476-.649.822-.88a1 1 0 01.812-.134zm.364 13.087A2.998 2.998 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879.586.585.879 1.353.879 2.121s-.293 1.536-.879 2.121z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                    <span
-                        class="leading-10 text-blue-800 text-2xl font-bold ml-1 uppercase"
-                        >TCIMS</span
-                    >
+                <a href="/portal" class="inline-flex flex-row items-center">
+                    <img src={logo}/>
                 </a>
             </div>
         </div>
         <div class="sidebar-content px-4 py-6">
             <ul class="flex flex-col w-full">
-                <li class="my-px">
-                    <a
-                        href="#"
-                        class="flex flex-row items-center h-10 px-3 rounded-lg text-black bg-gray-100"
-                    >
-                        <span
-                            class="flex items-center justify-center text-lg text-gray-400"
-                        >
-                            <svg
-                                fill="none"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                class="h-6 w-6"
-                            >
-                                <path
-                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                />
-                            </svg>
-                        </span>
-                        <span class="ml-3">Dashboard</span>
-                    </a>
-                </li>
+
                 <li class="my-px">
                     <span
                         class="flex font-medium text-sm text-black px-4 my-4 uppercase"
@@ -120,7 +84,7 @@
 
                 <li class="my-px">
                     <a
-                        href="#"
+                        href="/portal/make_payment"
                         class="flex flex-row items-center h-10 px-3 rounded-lg text-black-300 hover:bg-blue-800 hover:text-white"
                     >
                         <span
@@ -147,7 +111,7 @@
 
                 <li class="my-px">
                     <a
-                        href="#"
+                        href="/portal/make_claim"
                         class="flex flex-row items-center h-10 px-3 rounded-lg text-black-300 hover:bg-blue-800 hover:text-white"
                     >
                         <span
@@ -173,7 +137,7 @@
 
                 <li class="my-px">
                     <a
-                        href="#"
+                        href="/portal/track_claim"
                         class="flex flex-row items-center h-10 px-3 rounded-lg text-black-300 hover:bg-blue-800 hover:text-white"
                     >
                         <span
@@ -254,10 +218,6 @@
                             </svg>
                         </span>
                         <span class="ml-3">Notifications</span>
-                        <span
-                            class="flex items-center justify-center text-xs text-red-500 font-semibold bg-red-100 h-6 px-2 rounded-full ml-auto"
-                            >10</span
-                        >
                     </a>
                 </li>
 
---
+++ src/routes/(dashboard)/portal/make_payment/+page.svelte
@@ -0,0 +1,26 @@
+
+
+<form>
+    
+    
+    
+    
+    <div class="flex flex-wrap">
+
+        <div class="w-full  px-4 py-4">
+          <button
+            type="submit"
+            class="pl-40 pr-40 block w-full rounded-lg bg-blue-800 hover:bg-blue-700 transition px-5 py-3 text-sm font-medium text-white"
+          >
+            Make Payment via Existing Ncb Ammount
+          </button>
+
+          <p class="pt-3 text-center text-sm text-gray-500">
+            No account?
+            <a class="  text-blue-800 hover:underline " href="/signup">Signup</a>
+          </p>
+        </div>
+        
+      </div>
+
+</form>
