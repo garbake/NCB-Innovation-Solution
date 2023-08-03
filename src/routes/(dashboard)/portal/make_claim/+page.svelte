3480b258d1bc4c1a45780a770b39055038336ed2
Baker <garbake>
2023-08-03 16:20:38 -0500

    made changes to portal

--- src/routes/(dashboard)/portal/make_claim/+page.svelte
+++ src/routes/(dashboard)/portal/make_claim/+page.svelte
@@ -28,7 +28,7 @@
     <a href="#">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Victory Bread</h5>
     </a>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
     <a href="/portal/make_claim/claim" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-800 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
         Make Claim
         <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
--- src/routes/(dashboard)/portal/track_claim/+page.svelte
+++ src/routes/(dashboard)/portal/track_claim/+page.svelte
@@ -1,102 +1,60 @@
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-4 pt-4">
-    <div
-        class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:scale-105 transition w-full md:w-auto"
-    >
-        <a href="#">
-            <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-            >
-                Sigma Ltd
-            </h5>
-        </a>
+<div class="mx-auto max-w-lg text-center">
+    <h2 class="text-2xl font-bold text-blue-900 md:text-3xl">Tracking Claim</h2>
 
-        <div
-            class="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100"
-        >
-            <ol
-                class="relative z-10 flex justify-between text-sm font-medium text-gray-500"
-            >
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold"
-                    >
-                        1
-                    </span>
-
-                    <span class="hidden sm:block"> Application </span>
-                </li>
-
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white"
-                    >
-                        2
-                    </span>
-
-                    <span class="hidden sm:block"> Analyzing </span>
-                </li>
-
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold"
-                    >
-                        3
-                    </span>
-
-                    <span class="hidden sm:block"> Approval </span>
-                </li>
-            </ol>
-        </div>
-    </div>
-
-    <div
-        class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:scale-105 transition w-full md:w-auto"
-    >
-        <a href="#">
-            <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-            >
-                Sigma Ltd
-            </h5>
-        </a>
-
-        <div
-            class="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100"
-        >
-            <ol
-                class="relative z-10 flex justify-between text-sm font-medium text-gray-500"
-            >
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold"
-                    >
-                        1
-                    </span>
-
-                    <span class="hidden sm:block"> Details </span>
-                </li>
-
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white"
-                    >
-                        2
-                    </span>
-
-                    <span class="hidden sm:block"> Address </span>
-                </li>
-
-                <li class="flex items-center gap-2 bg-white p-2">
-                    <span
-                        class="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold"
-                    >
-                        3
-                    </span>
-
-                    <span class="hidden sm:block"> Payment </span>
-                </li>
-            </ol>
-        </div>
-    </div>
-
+    <p class="text-gray-500 sm:mt-2 sm:block">
+        Discover current stage of your claim .
+    </p>
 </div>
+
+    <div
+        class="max-w-xl ml-4  mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:scale-105 transition w-full md:w-auto"
+    >
+        <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+            Sigma Ltd
+        </h5>
+
+        <div
+            class="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100"
+        >
+            <ol
+                class="relative z-10 flex justify-between text-sm font-medium text-gray-500"
+            >
+                <li class="flex items-center gap-2 bg-white p-2">
+                    <span
+                        class="h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white"
+                    >
+                        1
+                    </span>
+
+                    <span class="sm:block"> Application </span>
+                </li>
+
+                <li class="flex items-center gap-2 bg-white p-2">
+                    <span
+                        class="h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white"
+                    >
+                        2
+                    </span>
+
+                    <span class="sm:block"> Analyzing </span>
+                </li>
+
+                <li class="flex items-center gap-2 bg-white p-2">
+                    <span
+                        class="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold"
+                    >
+                        3
+                    </span>
+
+                    <span class="sm:block"> Approval </span>
+                </li>
+            </ol>
+        </div>
+        
+    </div>
+    <div class="p-4">
+
+    </div>
+
